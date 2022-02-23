3a55aac20be7720b8cbb92b486ae70d6cbaa7249
Justin Conger <jhconger@GMAIL.COM>
2022-02-23 15:03:18 -0600

    add new items

---
+++ src/Ring_of_Marvol.java
@@ -0,0 +1,11 @@
+public class Ring_of_Marvol extends Item {
+    int accuracy = 60;
+    int damage = 60;
+
+    public Ring_of_Marvol(){
+    id = "Ring_of_Marvol";
+    name = "Ring of Marvol";
+    isWearable = true;
+    wearloc = "right finger";
+    }
+}
