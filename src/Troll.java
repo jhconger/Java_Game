1b4744bbca05728484e37280966eb15692eb6d92
Justin Conger <jhconger@GMAIL.COM>
2022-02-23 14:30:47 -0600

    adding weapons and NPC restructure with packages.

---
+++ src/Troll.java
@@ -0,0 +1,10 @@
+public class Troll  extends NPC{
+    public Troll() {
+        id = "Troll";
+        hp = 55;
+        accuracy = 25;
+        desc = "A Troll lumbers in front of you!";
+        name = "A Troll";
+    }
+}
+
